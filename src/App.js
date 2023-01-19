11358b8ea473cdd82f1319c2005afca575045629
Matthieu Mota <matthieumota@gmail.com>
2023-01-19 11:52:22 +0100

    Fix list

--- src/App.js
+++ src/App.js
@@ -13,11 +13,15 @@ function App() {
       <div className="results">
         <div className="beer">
           <h2>Beer 1</h2>
-          <img src="https://images.punkapi.com/v2/keg.png" alt="Beer 1" />
+          <img src="https://images.punkapi.com/v2/6.png" alt="Beer 1" />
         </div>
         <div className="beer">
           <h2>Beer 2</h2>
           <img src="https://images.punkapi.com/v2/2.png" alt="Beer 2" />
+        </div>
+        <div className="beer">
+          <h2>Beer 3</h2>
+          <img src="https://images.punkapi.com/v2/5.png" alt="Beer 3" />
         </div>
       </div>
     </div>
