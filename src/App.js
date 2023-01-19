b8b029c179f7c6d0d8fc44204e867bb33028af23
Matthieu Mota <matthieumota@gmail.com>
2023-01-19 11:25:13 +0100

    Search integration

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ function App() {
   return (
     <div className="container">
       <div className="header">
-        <h1>Beer App</h1>
+        <h1>Beer App!</h1>
+      </div>
+
+      <div className="search">
+        <input type="text" placeholder="Hoppy, Malt, Angry, New..." />
+        <button>Recherche</button>
       </div>
     </div>
   );
