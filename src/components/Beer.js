9ebadf310d2428709f9bcb813efefc0ce0019c91
Matthieu Mota <matthieumota@gmail.com>
2023-01-23 09:52:36 +0100

    Slugify

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import slugify from 'slugify';
 
 function Beer({ beer }) {
   return (
     <div className="beer">
-      <Link to={`/biere/${beer.id}/${beer.name}`}>
+      <Link to={`/biere/${beer.id}/${slugify(beer.name, { lower: true })}`}>
         <h2>{beer.name}</h2>
         <img src={beer.image_url} alt={beer.name} />
       </Link>
